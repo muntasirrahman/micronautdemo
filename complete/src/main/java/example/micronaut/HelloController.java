ab19697c77dcd8ef2be017e316a0da75ae5a491e
muntasirrahman <muntasir.rahman@quantuminventions.com>
2020-02-21 01:04:57 +0700

    New logging framework with file rotating handler.

--- complete/src/main/java/example/micronaut/HelloController.java
+++ complete/src/main/java/example/micronaut/HelloController.java
@@ -6,31 +6,25 @@ import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Produces;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.logging.FileHandler;
+import java.util.logging.Handler;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 @Controller("/hello") // <1>
 public class HelloController {
 
-    Logger logger = Logger.getLogger("example.micronaut");
-
-    public HelloController() {
-        FileHandler handler;
-        try {
-            handler = new FileHandler("default.log", true);
-            SimpleFormatter formatter = new SimpleFormatter();
-            handler.setFormatter(formatter);
-            logger.addHandler(handler);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+    private static LocalDateTime nextMidnight;
+    private static Logger logger = Logger.getLogger("example.micronaut");
 
     @Get
     @Produces(MediaType.TEXT_PLAIN)
     public String index() {
+        checkRotatingLogFilename();
         logger.info("Anonymous incoming request ");
         return "Hello World";
     }
@@ -38,7 +32,29 @@ public class HelloController {
     @Get("/{name}")
     @Produces(MediaType.TEXT_PLAIN)
     public String sayHello(String name) {
+        checkRotatingLogFilename();
         logger.info("Incoming request from " + name);
         return "Hello " + name;
+    }
+
+    private void checkRotatingLogFilename() {
+
+        LocalDateTime now = LocalDateTime.now();
+
+        if (nextMidnight == null || now.isAfter(nextMidnight)) {
+
+            nextMidnight = LocalDateTime.of(LocalDate.now(), LocalTime.MIDNIGHT).plusDays(1);
+            try {
+                for (Handler handler : logger.getHandlers()) logger.removeHandler(handler); //remove the existing one
+
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                FileHandler newFileHandler = new FileHandler("application" + formatter.format(now) + ".log", true);
+                newFileHandler.setFormatter(new SimpleFormatter());
+                logger.addHandler(newFileHandler);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
