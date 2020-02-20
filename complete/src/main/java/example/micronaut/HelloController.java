c4830c019997d09a02fbd30d6da5be2f906d55f8
muntasirrahman <muntasir.rahman@quantuminventions.com>
2020-02-20 22:07:47 +0700

    Greeting with name

--- complete/src/main/java/example/micronaut/HelloController.java
+++ complete/src/main/java/example/micronaut/HelloController.java
@@ -4,24 +4,41 @@ import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Produces;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 @Controller("/hello") // <1>
 public class HelloController {
 
-    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
-    @Get // <2>
-    @Produces(MediaType.TEXT_PLAIN) // <3>
+    Logger logger = Logger.getLogger("example.micronaut");
+
+    public HelloController() {
+        FileHandler handler;
+        try {
+            handler = new FileHandler("default.log", true);
+            SimpleFormatter formatter = new SimpleFormatter();
+            handler.setFormatter(formatter);
+
+            logger.addHandler(handler);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Get
+    @Produces(MediaType.TEXT_PLAIN)
     public String index() {
         logger.info("Anonymous incoming request ");
-        return "Hello World"; // <4>
+        return "Hello World";
     }
 
     @Get("/{name}")
     @Produces(MediaType.TEXT_PLAIN)
     public String sayHello(String name) {
-
         logger.info("Incoming request from " + name);
         return "Hello " + name;
     }
